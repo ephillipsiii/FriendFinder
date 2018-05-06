475b89b720afdee23511473ae229be4c821f6d36
Lappy Mc Lap Face <ephillipsiii>
2018-05-06 16:02:42 -0600

    added character to friends.js, added routing for html

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+module.exports = [{
+    "name": "Rick Sanchez",
+    "photo": "https://genius.com/albums/Kzlove/Rick-sanchez",
+    "answers": ["5", "5", "1", "5", "1", "1", "1", "3", "3", "3"]
+}];
