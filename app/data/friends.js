8af5663228194b21425336c8fade1307116de30e
Lappy Mc Lap Face <ephillipsiii>
2018-05-07 17:56:56 -0600

    updating routing paths for survey.html and _dirname

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,7 @@
-module.exports = [{
+var friendsArray = [{
     name: "Rick Sanchez",
     photo: "https://genius.com/albums/Kzlove/Rick-sanchez",
     answers: ["5", "5", "1", "5", "1", "1", "1", "3", "3", "3"]
 }];
+
+module.exports = friendsArray;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,12 @@ module.exports = function(app) {
 
     // GET route for /survey returns survey.html.
     app.get('/survey', function(req, res) {
-        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 
     // USE route returns home.html for any undefined GET routes.
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
 };
