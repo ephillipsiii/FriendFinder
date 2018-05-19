b61c0fec3ed008e22611f1efc2248ca9612db92f
Lappy Mc Lap Face <ephillipsiii>
2018-05-19 14:31:51 -0600

    solved api routing issues (was a scoping issue) - friends array now properly updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,12 @@ module.exports = function(app) {
 	app.get('/api/friends', function(req, res) {
 		res.json(friends);
     });
-}
+
 	app.post('/api/friends', function(req, res){
 		var userInput = req.body;
 		var userResponse = userInput.choices;
-	})
+		friends.push(userInput);
+	});
+	};
+
+	
--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
